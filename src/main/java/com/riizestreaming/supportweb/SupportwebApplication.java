5fe7b7c5c695e3bb7ed7957e3e452814e97290b6
gihap01 <howwonderful10000@gmail.com>
2025-03-30 22:32:28 +0900

    초기세팅
    초기세팅

---
+++ src/main/java/com/riizestreaming/supportweb/SupportwebApplication.java
@@ -0,0 +1,13 @@
+package com.riizestreaming.supportweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportwebApplication.class, args);
+	}
+
+}
